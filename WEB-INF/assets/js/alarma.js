7d8c5cb38d72e66e562ea4175aeb9ad81d90503a
gonzalojmartin <gonzalojmartin@hotmail.com>
2016-06-02 14:07:32 -0300

    web client

--- WEB-INF/assets/js/alarma.js
+++ WEB-INF/assets/js/alarma.js
@@ -2,6 +2,7 @@
  * Created by gmartin on 01/06/2016.
  * En el presente archivo JS se implementaran las peticiones GET contra el WS
  */
+var status = false;
 var URL_WS_ALARM_STATUS = "http://192.168.1.72:8080/app/rest/alarm/status";
 var URL_WS_ALARM_ON = "http://192.168.1.72:8080/app/rest/alarm/on";
 var URL_WS_ALARM_OFF = "http://192.168.1.72:8080/app/rest/alarm/off";
@@ -9,8 +10,24 @@ var URL_WS_TEMP_READ = "http://192.168.1.72:8080/app/rest/temperature/read";
 var URL_WS_TEMP_LIMITS = "http://192.168.1.72:8080/app/rest/temperature/limits";
 var URL_WS_TEMP_SET_LIMITS = "http://192.168.1.72:8080/app/rest/temperature/setlimits";
 
+
+$(document).ready(function () {
+
+    getAlarmStatusFromWS();
+
+    var elem = document.querySelector('.js-switch');
+    new Switchery(elem, { color: '#1AB394' });
+
+    setInterval( function() {
+        if(status){
+            getTemperatureValueFromWS();
+        }
+    }, 1500);
+
+});
+
 function switchOnOff(){
-    var status = $("#switch_on_off").is();
+    status = $("#switch_on_off").is();
     if(status) {
         powerOnAlarm();
     } else {
